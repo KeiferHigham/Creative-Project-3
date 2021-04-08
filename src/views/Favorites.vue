fc1f54c323d74defc46410c1993eb7b911ce3b30
Keifer Higham <keifer.higham22@gmail.com>
2021-04-08 12:55:04 -0600

    added editing functionality

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -4,6 +4,8 @@
     <div id="favorites">
       <div class="movies" v-for="(movie, index) in Favorites" :key="movie.id">
         <div class="movie">
+        <input type="text" placeholder ="Edit Title" v-model="newTitle"/>
+    <button @click="Edit(index)">Edit Title</button>
           <h2>{{movie.fullTitle}}</h2>
           <div class="movieGroup">
           <img :src="movie.image">
@@ -21,12 +23,17 @@ export default {
  name: 'Favorites',
  data: function() {
    return {
-     Favorites: this.$root.$data.Favorites
+     Favorites: this.$root.$data.Favorites,
+     newTitle: ""
    }
  },
   methods: {
     Remove (movieIndex) {
       this.$root.$data.Favorites.splice(movieIndex, 1);
+    },
+    Edit (movieIndex) {
+    this.$root.$data.Favorites[movieIndex].fullTitle = this.newTitle;
+    this.newTitle = ""
     }
   }
 }
